97e486c887b87e76e88736feefde7927716b836d
HANDE <handesekmen7@gmail.com>
2021-12-26 20:46:16 +0100

    second day work

--- src/test/java/featureAwork/featureA_Work.java
+++ src/test/java/featureAwork/featureA_Work.java
@@ -3,5 +3,6 @@ package featureAwork;
 public class featureA_Work {
     public static void main(String[] args) {
         System.out.println("new feature");
+        System.out.println("doing my second day work");
     }
 }
